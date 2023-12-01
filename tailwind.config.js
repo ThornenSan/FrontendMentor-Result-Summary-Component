7abf419e35b0fce6f6fa10be7e9035f0a0506de6
nathanxd404 <146950248+nathanxd404@users.noreply.github.com>
2023-12-01 19:49:57 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "375px",
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        // Primary Color
+        lightRed: "hsl(0, 100%, 67%)",
+        orangeyYellow: "hsl(39, 100%, 56%)",
+        greenTeal: "hsl(166, 100%, 37%)",
+        cobaltBlue: "hsl(234, 85%, 45%)",
+        // Gradients
+        lightSlateBlue: "hsl(252, 100%, 67%)",
+        lightRoyalBlue: "hsl(241, 81%, 54%)",
+        violetBlue: "hsla(256, 72%, 46%)",
+        persianBlue: "hsla(241, 72%, 46%, 0)",
+        // Neutral
+        paleBlue: "hsl(221, 100%, 96%)",
+        lightLavender: "hsl(241, 100%, 89%)",
+        darkGrayBlue: "hsl(224, 30%, 27%)",
+      },
+      fontFamily: {
+        sans: ["Hanken Grotesk", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
